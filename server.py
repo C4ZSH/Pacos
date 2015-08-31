8f8e3526511811e7692f6f017395d407429bba25
Aziz S <aziz@alpacalyp.se>
2015-08-30 21:32:54 -0400

    interactive config generation function for firstrun

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, g
 import argparse
-import os
+import os, sys
 import sqlite3
 import time, base64, hashlib
 
@@ -33,6 +33,23 @@ def firstrun():
                 schema = schm.read()
             conn.executescript(schema)
             conn.commit()
+
+def confgen():
+    mkconf = input("No config file detected. Press enter to generate one interactively, or ^C to exit and create one by hand using sampleconf.json as a template\n")
+    if mkconf = 'exit': sys.exit(0)
+    host = input("If you are unsure what to enter, pressing return will use reasonable defaults\nHostname to listen on (default: all): ")
+    v_host = '0.0.0.0' if host == '' else host
+    port = input("Port to listen on (default: 80): ")
+    if port != '' and (not port.isdigit() or int(port) < 65536):
+        port = input("Please enter a valid port, or press enter to use default: ")
+    v_port = 80 if port == '' else port
+    uploaddir = input("Path to directory for user-uploaded files (default: sthen/uploads/): ")
+    v_uploaddir = 'uploads' if uploaddir == '' else uploaddir
+    maxsize = input("Maximum allowed file size for uploads in bytes, or use suffix K, M, or G (default: 50 MB): ")
+    v_maxsize = 50 * 1024 * 1024
+    ssufx = {'K': 1024, 'M': 1024*1024, 'G': 1024**3}
+    if maxsize.strip()[-1:].upper() in ssufx and maxsize.strip()[:-1].isdigit(): 
+        v_maxsize = int(maxsize.strip()[:-1]) * ssufx[maxsize.strip()[-1:].upper()]
 
 def conn():
     return sqlite3.connect(DB_PATH)
